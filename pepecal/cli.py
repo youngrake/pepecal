be72d2c4d1c7f4036e21db82a17f1766b141cbbb
youngrake <youngrake@protonmail.com>
2021-10-01 14:28:57 +0300

    Initial setup

---
+++ pepecal/cli.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def main():
+    print("Pepe")
+    sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name="pepecal", author="youngrake", license="MIT", packages=["pepecal"])
